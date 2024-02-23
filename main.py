9f7d5e3245fc405e097bf6304cb814845e3345f3
Sanya <160408798+LovkiiSanya@users.noreply.github.com>
2024-02-23 18:33:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import schedule
 import datetime
+import random
 
 task_dict = {}
 
 
+def rndnumb():
+    numb = random.randint(1, 1000000)
+    return numb
+
+
 class Task:
-    def __init__(self, name, deadline, type, status):
+    def __init__(self, name, deadline, status):
         self.name = name
         self.deadline = deadline
-        self.type = type
+
         self.status = status
 
     def taskname():
@@ -25,7 +31,7 @@ class Task:
 
     def tasktype():
         print(
-            "Привет,выбери категорию заданий :(Бытовые дела = 1),(Тренировка = 2),(Домашняя работа = 3)"
+            "Выбери категорию заданий :(Бытовые дела = 1),(Тренировка = 2),(Домашняя работа = 3)"
         )
         type = int(input())
         if 1 <= type <= 3:
@@ -58,9 +64,36 @@ class cleaning(Task):
         super().__init__(name, deadline, status)
         self.item = item
 
+    def task_info():
+        task_id = rndnumb()
+        item_cleaning = cleaning.kindofitem()
+        task_deadline = Task.taskdeadline()
+        if task_deadline - datetime.datetime.now() <= datetime.timedelta(seconds=0):
+            cleaning_status = "Задача просрочена, фу"
+        else:
+            cleaning_status = "Задача активна, поднажмём"
+
+        task_cleaning = cleaning(
+            task_name,
+            task_deadline.strftime("%Y-%m-%d-%H.%M.%S"),
+            item_cleaning,
+            cleaning_status,
+        )
+        task_dict[task_id] = task_cleaning
+        print(task_dict)
+
     def kindofitem():
         item = input("Какой предмет или комнату будем приводить в порядок?")
         return item
+
+    def __repr__(self):
+        cleaning = type(self).__name__
+        return (
+            f"{cleaning}({self.name!r},{self.deadline!r},{self.item!r},{self.status!r})"
+        )
+
+    def __str__(self):
+        return f"Задание: {self.name} Срок годности: {self.deadline} Комната/предмет: {self.item} Статус: {self.status}"
 
 
 class training(Task):
@@ -68,9 +101,33 @@ class training(Task):
         super().__init__(name, deadline, status)
         self.muscle = muscle
 
+    def task_info():
+        task_id = rndnumb()
+        muscle_training = training.kindofmuscle()
+        task_deadline = Task.taskdeadline()
+        if task_deadline - datetime.datetime.now() <= datetime.timedelta(seconds=0):
+            cleaning_status = "Задача просрочена, фу"
+        else:
+            cleaning_status = "Задача активна, поднажмём"
+        task_training = training(
+            task_name,
+            task_deadline.strftime("%Y-%m-%d-%H.%M.%S"),
+            muscle_training,
+            cleaning_status,
+        )
+        task_dict[task_id] = task_training
+        print(task_dict)
+
     def kindofmuscle():
         muscle = input("Какую группу мышц прорабатываем?: ")
         return muscle
+
+    def __repr__(self):
+        training = type(self).__name__
+        return f"{training}({self.name!r},{self.deadline!r},{self.muscle!r},{self.status!r})"
+
+    def __str__(self):
+        return f"Задание: {self.name} Срок годности: {self.deadline} Мышца: {self.muscle} Статус: {self.status}"
 
 
 class student(Task):
@@ -78,18 +135,83 @@ class student(Task):
         super().__init__(name, deadline, status)
         self.homework = homework
 
+    def task_info():
+        task_id = rndnumb()
+        homework_task = student.kindofhomework()
+        task_deadline = Task.taskdeadline()
+        if task_deadline - datetime.datetime.now() <= datetime.timedelta(seconds=0):
+            cleaning_status = "Задача просрочена, фу"
+        else:
+            cleaning_status = "Задача активна, поднажмём"
+        student_task = student(
+            task_name,
+            task_deadline.strftime("%Y-%m-%d-%H.%M.%S"),
+            homework_task,
+            cleaning_status,
+        )
+        task_dict[task_id] = student_task
+        print(task_dict)
+
     def kindofhomework():
         homework = input("Какие уроки надо сделать?:")
         return homework
+
+    def __repr__(self):
+        student = type(self).__name__
+        return f"{student}({self.name!r},{self.deadline!r},{self.homework!r},{self.status!r})"
+
+    def __str__(self):
+        return f"Задание: {self.name} Срок годности: {self.deadline} Комната/предмет: {self.homework} Статус: {self.status}"
 
 
 while True:
 
     task_name = Task.taskname()
     print("Отлично, пусть будет", task_name)
-    dead_line = Task.taskdeadline()
-    print("Задание ", task_name, "записано на", dead_line)
     task_type = Task.tasktype()
     if task_type == 1:
-        cleaning.kindofitem()
-        Task.status_check()
+        cleaning.task_info()
+    if task_type == 2:
+        training.task_info()
+    if task_type == 3:
+        student.task_info()
+
+    CRUDoperation = int(
+        input(
+            "Если хотите просмотреть имеющиеся задачи нажмите(1), чтобы удалить(2), или изменить(3),закрыть ебейшее приложение(0)"
+        )
+    )
+    if CRUDoperation == 1:
+        check_tusk = int(input("хотите посмотреть все задачи?(1),по конкретному ID(2)"))
+        if check_tusk == 1:
+            print(task_dict)
+        if check_tusk == 2:
+            check_tusk_id = int(input("Введите ваш ID:"))
+            if check_tusk_id in task_dict:
+                print(task_dict.get(check_tusk_id))
+            else:
+                print("Такого ID в базе нет!")
+    if CRUDoperation == 2:
+        del_tusk = int(input("Введите ID задания которое надо удалить: "))
+        task_dict.pop(del_tusk)
+        print("Удалено!БимБим БомБом")
+
+    if CRUDoperation == 3:
+        change_task = int(input("По какому ID меняем задание?: "))
+        if type(task_dict[change_task]) == cleaning:
+            task_name = Task.taskname()
+            print("Отлично, пусть будет", task_name)
+            task_type = Task.tasktype()
+            cleaning.task_info()
+        if type(task_dict[change_task]) == training:
+            task_name = Task.taskname()
+            print("Отлично, пусть будет", task_name)
+            task_type = Task.tasktype()
+            training.task_info()
+        if type(task_dict[change_task]) == student:
+            task_name = Task.taskname()
+            print("Отлично, пусть будет", task_name)
+            task_type = Task.tasktype()
+            student.task_info()
+    if CRUDoperation == 0:
+        break
